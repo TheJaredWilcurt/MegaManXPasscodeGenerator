9f5d9c5f06fd68077c69dd35dd99b01d403a06ac
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2020-08-11 17:09:12 -0400

    Update mmxreversepasscode.vue

--- vue/mmxreversepasscode.vue
+++ vue/mmxreversepasscode.vue
@@ -329,7 +329,7 @@ module.exports = {
       });
       return passcode;
     },
-    expectedPassscode: function () {
+    expectedPasscode: function () {
       return mmxPasscodeGenerator(this.stages) || [];
     }
   },
