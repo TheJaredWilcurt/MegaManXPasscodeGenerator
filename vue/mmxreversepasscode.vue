4557f67f0f22bfd27547f4e362f9b532080db15f
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2020-08-11 16:05:19 -0400

    Update mmxreversepasscode.vue

--- vue/mmxreversepasscode.vue
+++ vue/mmxreversepasscode.vue
@@ -301,7 +301,7 @@ module.exports = {
     passcode: function () {
       let passcode = this.password.split('');
       passcode = passcode.filter(function (digit) {
-        return ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].inlcudes(digit);
+        return ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(digit);
       });
       passcode = passcode.map(function (digit) {
         return Number(digit);
