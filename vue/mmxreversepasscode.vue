970df071696af5683f699865514f6aa72c600314
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2020-08-11 17:04:50 -0400

    Update mmxreversepasscode.vue

--- vue/mmxreversepasscode.vue
+++ vue/mmxreversepasscode.vue
@@ -302,38 +302,19 @@ module.exports = {
       this.p11();
       this.p12();
     },
-    parseUrlParams: function () {
-      // '?passcode=1111-1111-1111&generate=false' => 'passcode=1111-1111-1111&generate=false'
-      let paramsUrl = location.search.replace('?', '');
-      // 'passcode=1111-1111-1111&generate=false' => ['passcode=1111-1111-1111', 'generate=false']
-      let paramPairs = paramsUrl.split('&');
-
-      let params = {};
-
-      // ['passcode=1111-1111-1111', 'generate=false'].forEach
-      paramPairs.forEach(function (paramPair) {
-        // 'generate=false' => 'generate'
-        let key = paramPair.split('=')[0];
-        // 'generate=false' => 'false'
-        let value = paramPair.split('=')[1];
-
-        if (value === 'false') {
-          params[key] = false;
-        } else if (value === 'true') {
-          params[key] = true;
-        } else {
-          params[key] = value;
-        }
-      });
-
-      if (params.passcode) {
+    setDataFromUrlParams: function () {
+      let params =  mmxHelpers.parseUrlParams();
+      if (typeof(params.generate) === 'boolean') {
         this.password = params.passcode;
       }
     },
     setUrlParams: function () {
       if (history.pushState) {
-        let newurl = window.location.protocol + '//' + window.location.host + window.location.pathname + '?passcode=' + this.password;
-        window.history.pushState({ path: newurl }, '', newurl);
+        let params = mmxHelpers.parseUrlParams();
+
+        params.passcode = this.password;
+
+        mmxHelpers.pushNewUrl(params);
       }
     }
   },
@@ -349,7 +330,7 @@ module.exports = {
       return passcode;
     },
     expectedPassscode: function () {
-      return mmxPasscodeGenerator(this.stages);
+      return mmxPasscodeGenerator(this.stages) || [];
     }
   },
   watch: {
@@ -361,7 +342,7 @@ module.exports = {
     }
   },
   created: function () {
-    this.parseUrlParams();
+    this.setDataFromUrlParams();
   }
 };
 </script>
